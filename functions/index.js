7714308948119eed6b9143ad098bccbe2098915b
utchak160 <chakravartyutkarsh@gmail.com>
2020-07-06 21:42:34 +0530

    create done

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./permission.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://node-test-b7812.firebaseio.com"
+});
+const db = admin.firestore();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({origin: true}));
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Hello World!');
+});
+
+app.post('/api/create', (req, res, next) => {
+    (async () => {
+        try {
+            const item = await db.collection('items').doc('/' + req.body.id + '/')
+                .create({item: req.body.item});
+            return res.status(201).send(item);
+        } catch (e) {
+            console.log(e);
+            return res.status(500).send(e);
+        }
+    })();
+})
+
+exports.app = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
